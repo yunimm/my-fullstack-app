7f16c3503eabdc5b41bbf23c746e000100e8f480
yunimm <yuu9916@gmail.com>
2024-02-18 20:27:34 +0800

    Add data to a database table

---
+++ app/components/Post.jsx
@@ -0,0 +1,10 @@
+export default function Post({ id, title, content, authorName }) {
+    return (
+        <div>
+            <p>dsfsd</p>
+            <h3>{authorName}</h3>
+            <h4>{title}</h4>
+            <p>{content}</p>
+        </div>
+    );
+}
